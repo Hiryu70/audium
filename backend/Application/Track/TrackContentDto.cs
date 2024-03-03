ec71cc6daa9fc60ad566f1832e8d40f211951637
NickGLn <42824853+NickGLn@users.noreply.github.com>
2024-03-03 14:15:15 +0300

    Delete backend/Application directory

--- backend/Application/Track/TrackContentDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Audium.WebApi;
-
-public class TrackContentDto
-{
-    public long TrackId { get; set; }
-    public byte[]? Content { get; set; }
-}
--- backend/Application/Tracks/AristsTracksTitleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Audium.Domain;
-
-namespace Audium.Application.Tracks;
-
-public class AristsTracksTitleDto
-{
-    public string TrackName { get; set; }
-    public string ArtistName { get; set; }
-}
